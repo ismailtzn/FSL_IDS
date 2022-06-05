22248676010bef0a893c1f0b52ecab7c4121cc55
İsmail Tüzün <evliyali.tzn@gmail.com>
2022-06-05 17:14:07 +0300

    file name bug fixed

--- dashboard/FSL_CICIDS2017/publish_results.sh
+++ dashboard/FSL_CICIDS2017/publish_results.sh
@@ -2,6 +2,6 @@
 dt=$(date '+%Y_%m_%d--%H_%M_%S');
 tar -cf "experiment_$dt.tar" run_experiments.log runs models individual_logs run_experiments_logs  test_parameters.txt
 mv experiment_$dt.tar ../../experiments
-git add ../../experiments experiment_$dt.tar
+git add ../../experiments/experiment_$dt.tar
 git commit -m "Added experiment $dt"
 git push
