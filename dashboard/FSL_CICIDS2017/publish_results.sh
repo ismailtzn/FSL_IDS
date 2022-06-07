e397cfb65ccf79679e5d29aaa425fd9704709fe1
İsmail Tüzün <evliyali.tzn@gmail.com>
2022-06-07 23:08:38 +0300

    Fixed name bug

--- dashboard/FSL_CICIDS2017/publish_results.sh
+++ dashboard/FSL_CICIDS2017/publish_results.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 dt=$(date '+%Y_%m_%d--%H_%M_%S');
-tar -czf experiment_$dt.tar.gz run_experiments.log runs models individual_logs run_experiments_logs $1
+tar -I 'gzip -9' -cf experiment_$dt.tar.gz run_experiments.log runs models individual_logs run_experiments_logs $1
 # tar -c --tape-length=50M --file=experiment_$dt.{00..50}.tar.gz run_experiments.log runs models individual_logs run_experiments_logs $1
-mv experiment_$dt.*.tar ../../experiments
-git add ../../experiments/experiment_$dt.*.tar
+mv experiment_$dt.tar.gz ../../experiments
+git add ../../experiments/experiment_$dt.tar.gz
 git commit -m "Added experiment $dt"
 git push
