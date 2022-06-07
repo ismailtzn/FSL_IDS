e4ff3bca902c53a6c41b9844074bf619d9d0bc90
İsmail Tüzün <evliyali.tzn@gmail.com>
2022-06-07 23:04:12 +0300

    Test parameters moved into another directory

--- dashboard/FSL_CICIDS2017/publish_results.sh
+++ dashboard/FSL_CICIDS2017/publish_results.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 dt=$(date '+%Y_%m_%d--%H_%M_%S');
-tar -c --tape-length=50M --file=experiment_$dt.{00..50}.tar run_experiments.log runs models individual_logs run_experiments_logs $1
+tar -czf experiment_$dt.tar.gz run_experiments.log runs models individual_logs run_experiments_logs $1
+# tar -c --tape-length=50M --file=experiment_$dt.{00..50}.tar.gz run_experiments.log runs models individual_logs run_experiments_logs $1
 mv experiment_$dt.*.tar ../../experiments
 git add ../../experiments/experiment_$dt.*.tar
 git commit -m "Added experiment $dt"
