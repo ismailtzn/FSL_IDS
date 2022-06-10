1adf0708e710b17217f1f9c4d62461df2c987e1d
İsmail Tüzün <evliyali.tzn@gmail.com>
2022-06-10 20:32:24 +0300

    Removed commented code

--- dashboard/FSL_CICIDS2017/publish_results.sh
+++ dashboard/FSL_CICIDS2017/publish_results.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 dt=$(date '+%Y_%m_%d--%H_%M_%S');
 tar -I 'gzip -9' -cf experiment_$dt.tar.gz run_experiments.log runs models individual_logs run_experiments_logs $1
-# tar -c --tape-length=50M --file=experiment_$dt.{00..50}.tar.gz run_experiments.log runs models individual_logs run_experiments_logs $1
 mv experiment_$dt.tar.gz ../../experiments
 git add ../../experiments/experiment_$dt.tar.gz
 git commit -m "Added experiment $dt"
