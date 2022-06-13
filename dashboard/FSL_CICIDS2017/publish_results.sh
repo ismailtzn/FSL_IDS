109a2c2f04e4e95a5a4853719d60389bd4875438
İsmail Tüzün <evliyali.tzn@gmail.com>
2022-06-13 18:26:59 +0300

    Added pull command before adding new item

--- dashboard/FSL_CICIDS2017/publish_results.sh
+++ dashboard/FSL_CICIDS2017/publish_results.sh
@@ -2,6 +2,7 @@
 dt=$(date '+%Y_%m_%d--%H_%M_%S');
 tar -I 'gzip -9' -cf experiment_$dt.tar.gz run_experiments.log runs history models individual_logs run_experiments_logs $1
 mv experiment_$dt.tar.gz ../../experiments
+git pull
 git add ../../experiments/experiment_$dt.tar.gz
 git commit -m "Added experiment $dt"
 git push
