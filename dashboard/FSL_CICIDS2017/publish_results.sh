71fbb4b9c102faab6d76ff5932c14710757995e2
İsmail Tüzün <evliyali.tzn@gmail.com>
2022-06-15 18:00:16 +0300

    tar command fixed

--- dashboard/FSL_CICIDS2017/publish_results.sh
+++ dashboard/FSL_CICIDS2017/publish_results.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 dt=$(date '+%Y_%m_%d--%H_%M_%S');
-tar -I 'gzip -9' -cf experiment_$dt.tar.gz run_experiments.log runs history models individual_logs run_experiments_logs $1
+hostInfo=$(hostname)
+tar -cvf experiment_$dt.tar.gz run_experiments.log runs history models individual_logs run_experiments_logs $1
 mv experiment_$dt.tar.gz ../../experiments
 git pull
 git add ../../experiments/experiment_$dt.tar.gz
-git commit -m "Added experiment $dt"
+git commit -m "Added experiment $dt from $hostInfo"
 git push
