bebcb08698897aac2248aeb0ae576c0f3568b180
İsmail Tüzün <evliyali.tzn@gmail.com>
2022-06-21 16:47:26 +0300

    Added filename to run.

--- dashboard/FSL_CICIDS2017/run_experiments.sh
+++ dashboard/FSL_CICIDS2017/run_experiments.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-if [[ $# -ge 1 ]]; then
-  input=$1
+if [[ $# -ge 2 ]]; then
+  scriptFile=$1
+  input=$2
   lineCount=$(wc -l < $input)
   echo Running test using $input, test count: $lineCount
 else
   echo Please provide text file that contains config parameters
   echo Run this script using following command
-  echo "nohup ./run_experiments.sh {PARAMETERS_FILE} > run_experiments.log 2>&1 &"
+  echo "nohup ./run_experiments.sh {test_script} {PARAMETERS_FILE} > run_experiments.log 2>&1 &"
   exit
 fi
 
@@ -17,8 +18,8 @@ i=0
 while IFS= read -r line
 do
   i=$((i+1))
-  echo "[$i/$lineCount] -> ./basic_train.py $line"
-  python3 basic_train.py $line > "run_experiments_logs/experiment_$i.log" 2>&1
+  echo "[$i/$lineCount] -> python3 $scriptFile $line"
+  python3 $scriptFile $line > "run_experiments_logs/experiment_$i.log" 2>&1
 done < "$input"
 
 ./publish_results.sh $input
