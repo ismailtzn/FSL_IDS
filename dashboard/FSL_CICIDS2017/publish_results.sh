b695ab0315cf070cf8d3624dca345fa3775dfa7d
İsmail Tüzün <evliyali.tzn@gmail.com>
2022-06-28 10:37:39 +0300

    Added current dir to commit message

--- dashboard/FSL_CICIDS2017/publish_results.sh
+++ dashboard/FSL_CICIDS2017/publish_results.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 dt=$(date '+%Y_%m_%d--%H_%M_%S');
 hostInfo=$(hostname)
+current_dir=$(pwd)
 tar -cjvf experiment_$dt.tar.gz run_experiments.log runs history models individual_logs run_experiments_logs $1
 mv experiment_$dt.tar.gz ../../experiments
 git pull
 git add ../../experiments/experiment_$dt.tar.gz
-git commit -m "Added experiment $dt from $hostInfo"
+git commit -m "Added experiment $dt from $hostInfo -- current dir: $current_dir"
 git push
