76bda211d810d3a5316d10320e64720c6042504f
İsmail Tüzün <evliyali.tzn@gmail.com>
2022-07-01 16:32:15 +0300

    base ann test configs updated

--- dashboard/FSL_CICIDS2017/base_ann_test/run_base_test.py
+++ dashboard/FSL_CICIDS2017/base_ann_test/run_base_test.py
@@ -5,7 +5,8 @@ import shutil
 import sys
 
 import pandas as pd
-from sklearn.metrics import classification_report, accuracy_score, f1_score
+from sklearn.metrics import classification_report, accuracy_score, f1_score, confusion_matrix
+from tabulate import tabulate
 
 import utility
 from attack_classifier import AttackClassifier
@@ -26,7 +27,8 @@ def create_result_dir(results_dir):
     logging.info('Created result directory: {}'.format(results_dir))
 
 
-def load_datasets(data_dir, hdf_key="cic_ids_2017"):
+def load_datasets(exp_config, hdf_key="cic_ids_2017"):
+    data_dir = exp_config['dataset_dir']
     x_train_files = glob.glob(data_dir + "/" + "x_meta_train*")
     y_train_files = glob.glob(data_dir + "/" + "y_meta_train*")
     x_test_files = glob.glob(data_dir + "/" + "x_meta_test*")
@@ -44,12 +46,21 @@ def load_datasets(data_dir, hdf_key="cic_ids_2017"):
     assert len(x_train_files) > 0
     assert len(y_train_files) > 0
 
-    x_train_files.extend(x_test_files)
-    y_train_files.extend(y_test_files)
     x_train_dfs = [pd.read_hdf(file, hdf_key) for file in x_train_files]
     y_train_dfs = [pd.read_hdf(file, hdf_key) for file in y_train_files]
+    x_test_dfs = [pd.read_hdf(file, hdf_key) for file in x_test_files]
+    y_test_dfs = [pd.read_hdf(file, hdf_key) for file in y_test_files]
     x_val_dfs = [pd.read_hdf(file, hdf_key) for file in x_val_files]
     y_val_dfs = [pd.read_hdf(file, hdf_key) for file in y_val_files]
+
+    x_test_df = pd.concat(x_test_dfs)
+    y_test_df = pd.concat(y_test_dfs)
+
+    y_test_df = y_test_df.groupby(y_test_df).sample(n=exp_config["k"])
+    x_test_df = x_test_df.loc[y_test_df.index]
+
+    x_train_dfs.append(x_test_df)
+    y_train_dfs.append(y_test_df)
 
     x_train = pd.concat(x_train_dfs)
     x_val = pd.concat(x_val_dfs)
@@ -66,23 +77,26 @@ def evaluate_intrusion_detector(classifier, X_test, y_test, label_encoder):
     accuracy = accuracy_score(y_test, y_test_pred)
     f1_weighted = f1_score(y_test, y_test_pred, average='weighted')
     report = classification_report(y_test, y_test_pred, output_dict=True, zero_division=0)
-    report.pop('accuracy')
-    report.pop('macro avg')
-    report.pop('weighted avg')
+    cf_matrix = confusion_matrix(y_test, y_test_pred)
 
     general_info = {'accuracy': accuracy, 'f1_score': f1_weighted}
     detailed_info = {}
 
     for class_name in report:
+        if class_name in ["accuracy", "macro avg", "weighted avg"]:
+            continue
         class_report = report[class_name]
         detailed_info[class_name] = class_report['f1-score']
 
-    evaluation_info = (general_info, detailed_info)
+    evaluation_info = (general_info, detailed_info, cf_matrix, report)
     return evaluation_info
 
 
-def write_report(result_dir, evaluation_info):
-    general_info, detailed_info = evaluation_info
+def write_report(result_dir, evaluation_info, classes):
+    general_info, detailed_info, cf_matrix, report = evaluation_info
+    classes = list(classes)
+    with open(result_dir + "/cf_matrix.csv", "w") as f:
+        f.write(tabulate(cf_matrix, tablefmt="plain", showindex=classes, headers=classes))
 
     general_df = pd.DataFrame(general_info, index=[0])
     detailed_df = pd.DataFrame(detailed_info, index=[0])
@@ -93,14 +107,14 @@ def write_report(result_dir, evaluation_info):
 
 def run_experiment(exp_config, classifier_config):
     # load dataset
-    datasets_orig = load_datasets(exp_config['dataset_dir'])
+    datasets_orig = load_datasets(exp_config)
     (X_train, y_train), (X_test, y_test) = datasets_orig
     y_train_enc, label_encoder = utility.encode_data(y_train)
 
     classifier = AttackClassifier(classifier_config)
     history = classifier.fit(X_train, y_train_enc)
     evaluation_info = evaluate_intrusion_detector(classifier, X_test, y_test, label_encoder)
-    write_report(exp_config['results_dir'], evaluation_info)
+    write_report(exp_config['results_dir'], evaluation_info, label_encoder.classes_)
     utility.plot_training_history(history, exp_config['results_dir'])
     utility.save_training_history(history, exp_config['results_dir'])
 
