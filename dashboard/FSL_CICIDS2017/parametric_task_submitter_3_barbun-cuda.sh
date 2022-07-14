db57c4169f3d9e63001513803ad0d515f6a2229c
İsmail Tüzün <evliyali.tzn@gmail.com>
2022-07-14 13:38:04 +0300

    updated

---
+++ dashboard/FSL_CICIDS2017/parametric_task_submitter_3_barbun-cuda.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -p barbun-cuda        	# Kuyruk adi: Uzerinde GPU olan kuyruk olmasina dikkat edin.
+#SBATCH -A ituzun         	    # Kullanici adi
+#SBATCH -J agent3_torch_experiment     # Gonderilen isin ismi
+#SBATCH --dependency singleton
+#SBATCH -o experiment_3.out    	# Ciktinin yazilacagi dosya adi
+#SBATCH --gres=gpu:1        	# Her bir sunucuda kac GPU istiyorsunuz? Kumeleri kontrol edin.
+#SBATCH -N 1                	# Gorev kac node'da calisacak?
+#SBATCH -n 1                	# Ayni gorevden kac adet calistirilacak?
+#SBATCH --cpus-per-task 20  	# Her bir gorev kac cekirdek kullanacak? Kumeleri kontrol edin.
+#SBATCH --time=1-23:00:00      	# Sure siniri koyun.
+#SBATCH --mail-user=e2036234@ceng.metu.edu.tr
+#SBATCH --mail-type=ALL
+
+eval "$(/truba/home/$USER/miniconda3/bin/conda shell.bash hook)"
+conda activate torch-env
+conda init
+
+cd /truba/home/$USER/agent3/FSL_IDS/dashboard/FSL_CICIDS2017/
+./run_experiments.sh $1 $2 $3 > run_experiments.log 2>&1
